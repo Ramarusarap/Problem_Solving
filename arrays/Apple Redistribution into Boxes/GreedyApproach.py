36aca788286cfda6edd5cda050440fdf368f002b
Ramarusarap <parasuramofficial@gmail.com>
2025-12-24 11:02:30 +0530

    Implement simplified minimum boxes solution
    Added a simplified solution for minimum boxes using a greedy approach.

--- arrays/Apple Redistribution into Boxes/GreedyApproach.py
+++ arrays/Apple Redistribution into Boxes/GreedyApproach.py
@@ -29,6 +29,22 @@ class Solution:
         # Number of boxes used
         return len(capacity) - i
 
+"""
+One simplified version I found in Leetcode's solution tab
+    class Solution:
+        def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
+            all_apples = sum(apple)
+            capacity.sort()
+            boxes = 0
+    
+            for c in capacity[::-1]:
+                if all_apples > 0:
+                    all_apples -= c
+                    boxes += 1
+                else:
+                    return boxes
+            return boxes"""
+
 ⏱ Time Complexity
 
 O(n + m log m)
